bd41ee5f5adfa5733cedbf19e9099f8fb08e1acc
Tushar Goel <tushar.goel@byjus.com>
2022-07-19 20:33:22 +0530

    test

--- src/CustomCard.jsx
+++ src/CustomCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Card,CardHeader,CardBody,CardText} from "reactstrap";
+ 
+//test 
 
 function CustomCard(props){
     return (
         <div>
             <Card>
                 <CardHeader style={{backgroundColor: "#ffc106"}}>Todo {props.id}</CardHeader>
-                <CardBody>
+                <CardBody> 
                     <CardText>User Id : {props.userId}</CardText>
                     <CardText>Completed: {props.completed ? "YES" : "NO"}</CardText>
                     <CardText>Title: {props.title}</CardText>
